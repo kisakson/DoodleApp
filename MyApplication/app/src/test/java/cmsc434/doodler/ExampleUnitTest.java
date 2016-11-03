09e03151e95f3df03c0dac81f48ce9b5f15ae3b4
kisakson <kara.isakson@gmail.com>
2016-11-02 22:20:11 -0400

    Import Android app structure

---
+++ MyApplication/app/src/test/java/cmsc434/doodler/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package cmsc434.doodler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
